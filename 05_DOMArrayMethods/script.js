8b9dd68b790d3d812ed5ab32f8387db30e958907
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-01-07 15:06:39 -0800

    33. Calculate Wealth - reduce()

--- 05_DOMArrayMethods/script.js
+++ 05_DOMArrayMethods/script.js
@@ -47,7 +47,16 @@ function showMillionaires() {
     data = data.filter(user => user.money > 1000000);
 
     updateDOM();
-}
+};
+
+// Calculate the total wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+};
 
 // Add new obj to data arr
 function addData(obj) {
@@ -78,4 +87,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
-showMillionairesBtn.addEventListener('click', showMillionaires)
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
