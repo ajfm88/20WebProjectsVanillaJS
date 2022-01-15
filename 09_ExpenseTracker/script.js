9838c83781cdfba03b146b514f11161fcf9ce832
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-01-15 08:59:36 -0800

    58. Finish Expense Tracker project

--- 09_ExpenseTracker/script.js
+++ 09_ExpenseTracker/script.js
@@ -1,5 +1,5 @@
 const balance = document.getElementById('balance');
-const money_plus = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
 const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
@@ -13,45 +13,48 @@ const amount = document.getElementById('amount');
 //   { id: 4, text: 'Camera', amount: 150 }
 // ];
 
-const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+const localStorageTransactions = JSON.parse(
+    localStorage.getItem('transactions')
+);
 
-let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+let transactions =
+    localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 // Add transaction
 function addTransaction(e) {
     e.preventDefault();
 
     if (text.value.trim() === '' || amount.value.trim() === '') {
-        alert('Please add a txt and amount');
+        alert('Please add a text and amount');
     } else {
-        const transaction = {
-            id: generateID(),
-            text: text.value,
-            amount: +amount.value
-        };
-
-        transactions.push(transaction);
-
-        addTransactionDOM(transaction);
-
-        updateValues();
-
-        updteLocalStorage();
-
-        text.value = '';
-        amount.value = '';
+    const transaction = {
+        id: generateID(),
+        text: text.value,
+        amount: +amount.value
     };
-};
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
 
 // Generate random ID
 function generateID() {
     return Math.floor(Math.random() * 100000000);
-};
+}
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
     // Get sign
-    const sign = transaction.amount < 0 ? '-' : '+'
+    const sign = transaction.amount < 0 ? '-' : '+';
 
     const item = document.createElement('li');
 
@@ -59,49 +62,58 @@ function addTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete=btn" onclick="removeTransaction(${transaction.id})">✕</button>`;
+        ${transaction.text} <span>${sign}${Math.abs(
+        transaction.amount
+    )}</span> <button class="delete-btn" onclick="removeTransaction(${
+        transaction.id
+    })">x</button>
+    `;
 
     list.appendChild(item);
-};
+}
 
 // Update the balance, income and expense
 function updateValues() {
     const amounts = transactions.map(transaction => transaction.amount);
-  
+
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-  
+
     const income = amounts
-      .filter(item => item > 0)
-      .reduce((acc, item) => (acc += item), 0)
-      .toFixed(2);
-  
-    const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
-  
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+        -1
+    ).toFixed(2);
+
     balance.innerText = `$${total}`;
     money_plus.innerText = `$${income}`;
     money_minus.innerText = `$${expense}`;
-};
+}
 
 // Remove transaction by ID
 function removeTransaction(id) {
     transactions = transactions.filter(transaction => transaction.id !== id);
 
-    updteLocalStorage();
+    updateLocalStorage();
 
     init();
-};
+}
 
 // Update local storage transactions
-function updteLocalStorage() {
+function updateLocalStorage() {
     localStorage.setItem('transactions', JSON.stringify(transactions));
-};
+}
 
 // Init app
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM);
-};
+    updateValues();
+}
 
 init();
 
