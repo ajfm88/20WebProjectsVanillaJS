57100f0485c1451ba2c5fe560c34185aaa71a035
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-01-15 20:04:52 -0800

    63. Next, Prev Song & Progress

--- 10_MusicPlayer/script.js
+++ 10_MusicPlayer/script.js
@@ -12,7 +12,7 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
-const songs = ['hey', 'summer', 'ukelele'];
+const songs = ['hey', 'summer', 'ukulele'];
 
 // Keep track of song
 let songIndex = 2;
@@ -45,6 +45,48 @@ function pauseSong() {
 	audio.pause();
 }
 
+// Previous song
+function prevSong() {
+	songIndex--;
+
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
+
+	loadSong(songs[songIndex]);
+
+	playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+	const { duration, currentTime } = e.srcElement;
+	const progressPercent = (currentTime / duration) * 100;
+	progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress() {
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
+
+	audio.currTime = (clickX / width) * duration;
+}
+
+// Next song
+function nextSong() {
+	songIndex++;
+
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
+
+	loadSong(songs[songIndex]);
+
+	playSong();
+}
+
 // Event listeners
 playBtn.addEventListener('click', () => {
 	const isPlaying = musicContainer.classList.contains('play');
@@ -55,3 +97,16 @@ playBtn.addEventListener('click', () => {
 		playSong();
 	}
 });
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
