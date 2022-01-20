607d4d4de35e0284ef2fafb4419606af56a5e8b4
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-01-20 14:01:18 -0800

    96. Finish Lyrics Search App

--- 15_LyricsSearch/script.js
+++ 15_LyricsSearch/script.js
@@ -65,14 +65,20 @@ async function getMoreSongs(url) {
 async function getLyrics(artist, songTitle) {
     const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
     const data = await res.json();
-
-    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
-
-    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
-    <span>${lyrics}</span>`;
-
+  
+     if (data.error) {
+          result.innerHTML = data.error;
+     } else {
+          const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+  
+          result.innerHTML = `
+              <h2><strong>${artist}</strong> - ${songTitle}</h2>
+              <span>${lyrics}</span>
+          `;
+    }
+  
     more.innerHTML = '';
-}
+  }
 
 // Event listeners
 form.addEventListener('submit', e => {
